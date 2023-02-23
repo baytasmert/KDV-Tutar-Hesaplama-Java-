11971cca5e3fb65210efd99bd216f5b22725a521
Mert Bayats <mertbaytas@gmail.com>
2023-02-23 13:31:59 +0300

    First

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("KDV Hesabı için para değeri giriniz: ");
+        Scanner oku= new Scanner(System.in);
+        int para=oku.nextInt();
+
+        double kdv1=0.18;
+        double kdv2=0.08;
+
+        double tutar = 0;
+
+        if (para<=1000 && para>0){
+            tutar+=para*kdv1;
+        } else if (para>1000) {
+            tutar+=para*kdv2;
+        }else {
+            System.out.println("Hatali bir miktar girdiniz!");
+            System.exit(1);
+        }
+
+        System.out.println("KDV'li fiyat = "+(para+tutar));
+        System.out.println("KDV miktari = "+ tutar);
+
+
+
+
+
     }
 }
